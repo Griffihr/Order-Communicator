a091a53e96c11e047a73c5b2a28db8797fa3bd00
Griffihr <45300435+Griffihr@users.noreply.github.com>
2023-04-07 11:44:08 +0300

    Cmnd_constrct

--- Command Transmission/MainWindow.xaml.cs
+++ Command Transmission/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,39 @@ using System.Windows.Shapes;
 
 namespace Command_Transmission
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+        private ObservableCollection<Command_Struct> CmdStrct = new ObservableCollection<Command_Struct>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //ObservableCollection<Command_Struct> CmdStrct = new ObservableCollection<Command_Struct>();
+
+            CmdStrct.Add(new Command_Struct() {StartTs = 0, Prio = 1});
+
+            DG1.ItemsSource = CmdStrct;
+        }
+
+        public class Command_Struct
+        {
+            public int MaxTid { get; set; }
+            public int AntalUpdr { get; set; }
+            public int MaxUpdrH { get; set; }
+            public int UppAddr { get; set; }
+            public int AvAddr { get; set; }
+            public int UppdrAntal { get; set; }
+            public int Prio { get; set; } 
+            public int StartTs { get; set; }
+            public int KördH { get; set; }
+
+        }
+
+        private void Add_button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
